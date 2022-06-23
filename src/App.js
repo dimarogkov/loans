c2e8b828ee973b9fef0c9759625d2f5de2fc12f4
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2022-06-23 13:52:27 +0300

    deploy fix

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import Loans from "./components/loans/Loans";
 import Error from "./components/error/Error";
 import Layout from "./hoc/Layout/layout";
 
-const App = () => {
-  return (
-      <Layout>
-          <Routes>
-              <Route exact path="/" element={ <Loans /> } />
-              <Route path="*" element={ <Error /> } />
-          </Routes>
-      </Layout>
-  );
-};
+const App = () => (
+  <Layout>
+      <Routes>
+          <Route exact path="/" element={ <Loans /> } />
+          <Route path="*" element={ <Error /> } />
+      </Routes>
+  </Layout>
+);
 
 export default App;
